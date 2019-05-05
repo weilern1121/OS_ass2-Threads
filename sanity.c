e0b7e5177c5529292c8af9779f09fa94ca3e5c56
weilern1121 <37665993+weilern1121@users.noreply.github.com>
2019-05-05 22:13:36 +0300

    Done sanity!

--- sanity.c
+++ sanity.c
@@ -1,11 +1,15 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
-#define NTHREADS       16  // max num of threads per proc
-volatile int kthreadsRunFlag; //flag for test 2.2
+#include "param.h"
+#include "tournament_tree.h"
+#include "kthread.h"
 
 /********** 2.1 tests  **********/
+
+volatile int kthreadsRunFlag; //flag for test 2.2
+#define NTHREADS       16  // max num of threads per proc
+
 //test fork,exit and wait
 int test_21_1(void) {
     int pid, i = 0, counter = 0;
@@ -255,42 +259,174 @@ void test_31(void) {
     }
 }
 
+
+/********** 3.2 tests  **********/
+
+
+trnmnt_tree *sanityTree;
+volatile int activeThread;
+volatile int forRun;
+
+void thread1Job() {
+    int acquire;
+
+    while (activeThread) {}
+
+    if ((acquire = trnmnt_tree_acquire(sanityTree, forRun)) < 0)
+        printf(1, "COULDNT ACUIRE TREE LOCK \n");
+
+    //NOW WE GO SLEEP AND TRY DEALLOC WITH THREAD2
+
+    sleep(500);
+
+    if ((acquire = trnmnt_tree_release(sanityTree, forRun)) < 0)
+        printf(1, "COULDNT RELEASE TREE LOCK\n");
+
+    //SAFE EXIT AFTER RELEASE LOCK
+
+    kthread_exit();
+}
+
+void thread2Job() {
+    int dealloc;
+    while (activeThread) {}
+
+    //WE WANT THREAD 1 TO ACQUIRE LOCK
+    sleep(200);
+
+    if ((dealloc = trnmnt_tree_dealloc(sanityTree)) == 0)
+        printf(1, "THREAD 1 HOLD LOCK, SHOULD NOT DEALLOC\n");
+
+    //WE WAIT FOR THREAD 1 TO RELEASE LOCK
+    sleep(1000);
+
+    if ((dealloc = trnmnt_tree_dealloc(sanityTree)) == -1)
+        printf(1, "THREAD 1 RELEASE LOCK, SHOULD DEALLOC\n");
+
+
+    //SAFE EXIT AFTER DEALLOC
+    kthread_exit();
+}
+
+
+int TOURNAMENT_TREE_TEST(void) {
+    int pid;
+    printf(1, "TESTING FOR 16 THREADS IN DEPTH 4\n");
+
+    if ((pid = fork()) == 0) {
+
+        for (int i = 0; i < 16; i++) {
+            forRun = i;
+            //THREADS SHOULD NOT RUN UNTIL ITS HAPPENING TOGETHER
+            activeThread = 1;
+
+            printf(1, "TEST ON %d RUN\n", forRun);
+
+            int pidA, pidB, join;
+            void *maxStack1 = ((char *) malloc(4000 * sizeof(char))) + 4000;
+            void *maxStack2 = ((char *) malloc(4000 * sizeof(char))) + 4000;
+
+            if ((sanityTree = trnmnt_tree_alloc(4)) == 0)
+                printf(1, "NO ALLOC TREE \n");
+
+            pidA = kthread_create(thread1Job, maxStack1);
+            pidB = kthread_create(thread2Job, maxStack2);
+
+            activeThread = 0;
+
+            printf(1, " JOIN ON THREAD 1 \n");
+            if ((join = kthread_join(pidA)) == -1)
+                printf(1, "COULDNT JOIN FOR SOME REASON \n");
+            else if (join == 0)
+                printf(1, "JOIN WORKED FOR THREAD 1\n");
+            else
+                printf(1, "BUGGGGG IN JOIN \n");
+
+            printf(1, " DONE JOIN ON THREAD 1 \n");
+            printf(1, " JOIN ON THREAD 2 \n");
+
+            if ((join = kthread_join(pidB)) == -1)
+                printf(1, "COULDNT JOIN FOR SOME REASON \n");
+            else if (join == 0)
+                printf(1, "JOIN WORKED FOR THREAD 1\n");
+            else
+                printf(1, "BUGGGGG IN JOIN \n");
+
+            printf(1, " DONE JOIN ON THREAD 2 \n");
+
+            printf(1, "TEST ON %d FINISH \n", forRun);
+        }
+        exit();
+    } else {
+        // WE WAIT FOR SON PROCESS TO CREATE TOURNAMENT TREE
+        printf(1, "FATHER PROC WAIT FOR SON");
+        wait();
+    }
+
+    printf(1, "FINISHED TEST IF NO BAD COMMENTS ALL GOOD");
+    exit();
+
+}
+
+
+void test_32(void) {
+    int pid;
+    if ((pid = fork()) == 0) {
+        TOURNAMENT_TREE_TEST();
+        exit();
+    } else if (pid > 0) { //wait for children to finish execution
+        sleep(500);
+        kill(pid);
+        wait();
+    } else {
+        printf(1, "fork failed\n");
+        exit();
+    }
+}
+
+
 /***************  main  ***************/
 
 
-    void run_test(int testNum) {
-        switch (testNum) {
-            case 21:
-                printf(1, "-- Start test 2.1 --\n");
-                if (test_21_1())
-                    test_21_2();
-                else
-                    printf(1, "-- Failed in test 2.1 --\n");
-                printf(1,"\n");
-                break;
-            case 22:
-                printf(1, "-- Start test 2.2 --\n");
-                test_22();
-                printf(1, "-- Test 2.2 Passed! --\n");
-                printf(1,"\n");
-                break;
-            case 31:
-                printf(1, "-- Start test 3.1 --\n");
-                test_31();
-                printf(1, "-- Test 3.1 Passed! --\n");
-                printf(1,"\n");
-                break;
-            default:
-                printf(2, "ERROR- wrong test_ID %d \n\n", testNum);
-                return;
-        }
+void run_test(int testNum) {
+    switch (testNum) {
+        case 21:
+            printf(1, "-- Start test 2.1 --\n");
+            if (test_21_1())
+                test_21_2();
+            else
+                printf(1, "-- Failed in test 2.1 --\n");
+            printf(1, "\n");
+            break;
+        case 22:
+            printf(1, "-- Start test 2.2 --\n");
+            test_22();
+            printf(1, "-- Test 2.2 Passed! --\n");
+            printf(1, "\n");
+            break;
+        case 31:
+            printf(1, "-- Start test 3.1 --\n");
+            test_31();
+            printf(1, "-- Test 3.1 Passed! --\n");
+            printf(1, "\n");
+            break;
+        case 32:
+            printf(1, "-- Start test 3.2 --\n");
+            test_32();
+            printf(1, "\n-- Test 3.2 Passed! --\n");
+            printf(1, "\n");
+            break;
+        default:
+            printf(2, "ERROR- wrong test_ID %d \n\n", testNum);
+            return;
     }
+}
 
-    int main(int argc, char *argv[]) {
-        run_test(21); //Test to part 2.1 - fork, exit, wait and exec
-        run_test(22);//Test for part 2.2 - kthread create, exit and join
-        run_test(31);//Test for part 3.1 - mutex alloc,dealloc,lock,unlock
+int main(int argc, char *argv[]) {
+    run_test(21); //Test to part 2.1 - fork, exit, wait and exec
+    run_test(22);//Test for part 2.2 - kthread create, exit and join
+    run_test(31);//Test for part 3.1 - mutex alloc,dealloc,lock,unlock
+    run_test(32);//Test for part 3.2 - trnmnt_tree alloc,dealloc,acquire, release
 
-
-        exit();
-    }
+    exit();
+}
